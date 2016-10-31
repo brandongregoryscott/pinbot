f29368312630be22344a3d87b62d7ed99b588745
Brandon Scott <brandongregoryscott@gmail.com>
2016-10-31 16:50:49 -0400

    Pinbot will now echo pinned posts for emphasis.

--- pinbot.py
+++ pinbot.py
@@ -1,6 +1,5 @@
 import os
 import time
-import json
 from slackclient import SlackClient
 
 BOT_ID = os.environ.get("BOT_ID")
@@ -13,11 +12,6 @@ EXAMPLE_COMMAND = "do"
 slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
 
 def handle_command(command, channel):
-    """
-        Receives commands directed at the bot and determines if they
-        are valid commands. If so, then acts on the commands. If not,
-        returns back what it needs for clarification.
-    """
     response = "Not sure what you mean. Use the *" + EXAMPLE_COMMAND + \
                "* command with numbers, delimited by spaces."
     if command.startswith(EXAMPLE_COMMAND):
@@ -27,24 +21,14 @@ def handle_command(command, channel):
 
 
 def parse_slack_output(slack_rtm_output):
-    """
-        The Slack Real Time Messaging API is an events firehose.
-        this parsing function returns None unless a message is
-        directed at the Bot, based on its ID.
-    """
     output_list = slack_rtm_output
     if output_list and len(output_list) > 0:
         for output in output_list:
-            print("DUMP: ")
-            print(output)
-            # print(output)
-            # if output and 'text' in output and AT_BOT in output['text']:
-            #     # return text after the @ mention, whitespace removed
-            #     return output['text'].split(AT_BOT)[1].strip().lower(), \
-            #            output['channel']
-            if output and 'text' in output:
-                print("Text: " + output['text'])
-                print("Channel: " + output['channel'])
+            if output['type'] == 'message' and output.has_key('subtype') and output['subtype'] == 'pinned_item':
+                slack_client.api_call('chat.postMessage',
+                    channel=output['channel'],
+                    attachments=output['attachments'],
+                    as_user=True)
     return None, None
 
 if __name__ == "__main__":
