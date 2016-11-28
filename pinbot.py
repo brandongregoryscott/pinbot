365c7c4c3b34ab689bb09990409c9d4ca5b1de2c
Brandon Scott <brandongregoryscott@gmail.com>
2016-11-28 09:57:28 -0500

    Fixed image pins crashing the bot

--- pinbot.py
+++ pinbot.py
@@ -163,7 +163,7 @@ def parse_slack_output(slack_rtm_output):
             print(str(output).encode('utf-8'))
             if output['type'] == 'message' and 'text' in output and AT_BOT in output['text']:
                 return output['text'].split(AT_BOT)[1].strip().lower(), output['channel']
-            if output['type'] == 'message' and 'subtype' in output and output['subtype'] == 'pinned_item':
+            if output['type'] == 'message' and 'subtype' in output and output['subtype'] == 'pinned_item' and 'attachments' in output:
                 slack_client.api_call('chat.postMessage',
                                       channel=output['channel'],
                                       attachments=output['attachments'],
