09327412f5f71bead31a25afff13af3641311cb8
Brandon Scott <contact@brandonscott.me>
2023-06-20 15:17:26 -0400

    Simplify pin message re-posting + image support (#23)

---
+++ src/utilities/config.ts
@@ -0,0 +1,33 @@
+import isEmpty from "lodash/isEmpty";
+import dotenv from "dotenv";
+
+if (process.env.ENV !== "production") {
+    // Load process.env values from .env file
+    dotenv.config();
+}
+
+const USERS: Record<string, string> = isEmpty(process.env.USERS)
+    ? {}
+    : JSON.parse(process.env.USERS!);
+
+const TOKENS: Record<string, string> = isEmpty(process.env.TOKENS)
+    ? {}
+    : JSON.parse(process.env.TOKENS!);
+
+const {
+    CHANNEL,
+    CLIENT_ID,
+    CLIENT_SECRET,
+    CLIENT_SIGNING_SECRET,
+    REDIRECT_URI,
+} = process.env;
+
+export {
+    CHANNEL,
+    USERS,
+    TOKENS,
+    CLIENT_ID,
+    CLIENT_SECRET,
+    CLIENT_SIGNING_SECRET,
+    REDIRECT_URI,
+};
